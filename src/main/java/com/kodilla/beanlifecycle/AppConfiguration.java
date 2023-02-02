c1068492b561d165ae8c2b5265c06288721c82dc
Pawel Buczek <pawel.buczek@email.com>
2023-02-02 20:54:03 +0100

    Initial commit

---
+++ src/main/java/com/kodilla/beanlifecycle/AppConfiguration.java
@@ -0,0 +1,26 @@
+package com.kodilla.beanlifecycle;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+class AppConfiguration {
+
+    @Bean
+    public LibraryManager libraryManager() {
+        return new LibraryManager();
+    }
+
+    @Bean
+    public SomeOtherBean someOtherBean() {
+        return new SomeOtherBean();
+    }
+
+    @Bean
+    public BeanMonitor beanMonitor() {
+        return new BeanMonitor();
+    }
+
+}
